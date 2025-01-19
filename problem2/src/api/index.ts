2d21a32c8e4f8e13bd018f3ef4feeb5b90a10db9
barlydjaja <barlydjaja@gmail.com>
2025-01-19 16:57:07 +0700

    feat: revert api coinPriceInfo to use proxy

--- problem2/src/api/index.ts
+++ problem2/src/api/index.ts
@@ -3,5 +3,5 @@ export { default } from './request.ts';
 export const ApiDelay = 500;
 
 export const endpointList = {
-  coinPriceInfo: 'https://interview.switcheo.com/prices.json'
+  coinPriceInfo: '/api/prices.json'
 };
